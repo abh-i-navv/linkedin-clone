d5f19232781a375e5f3c688949e84af1612fbfdb
abh-i-navv <abhinav11290@gmail.com>
2025-08-07 15:26:51 +0530

    landing page improvements

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,21 @@
-"use client"
+"use client";
+
 import Header from "@/components/Header";
 import PostForm from "@/components/PostForm";
 import UserInfo from "@/components/UserInfo";
 import UserPost from "@/components/UserPost";
 import { useSession } from "next-auth/react";
 
-
 export default function Home() {
   const session = useSession();
-  const user = session?.data?.user
+  const user = session?.data?.user;
 
-  if(!user) {
-    return (
-      <div className="flex items-center justify-center h-screen">
-        <h1 className="text-2xl font-bold">Please log in to view your profile</h1>
-      </div>
-    );
-  }
-
-  const data = {
-    name: user.name!,
-    bio: user.bio!
-  }
+  const data = user
+    ? {
+        name: user.name!,
+        bio: user.bio!,
+      }
+    : null;
 
   return (
     <>
@@ -30,21 +24,17 @@ export default function Home() {
       </header>
 
       <main>
-        <div className="grid grid-cols-8 mt-5 ">
+        <div className="grid grid-cols-8 mt-5">
           <section className="hidden md:inline md:col-span-2">
-            {<UserInfo user={data}/>}
+            {data && <UserInfo user={data} />}
           </section>
 
           <section className="col-span-full md:col-span-4 mx-auto w-full">
-            
             <PostForm />
             <UserPost />
-
           </section>
         </div>
       </main>
-      <div>
-      </div>
     </>
   );
 }
